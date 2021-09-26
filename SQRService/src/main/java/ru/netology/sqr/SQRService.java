b1f9d5b3511a27e96170c1a47115beae59a3a724
Serbatico <Serbatico@yandex.ru>
2021-09-26 23:01:12 +0300

    -v

--- SQRService/src/main/java/ru/netology/sqr/SQRService.java
+++ SQRService/src/main/java/ru/netology/sqr/SQRService.java
@@ -5,7 +5,8 @@ public class SQRService {
     public int sqrtCeil(int lowNum, int highNum) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= lowNum && i * i <= highNum) {
+            int sqrtNum = i * i;
+            if (sqrtNum >= lowNum && sqrtNum <= highNum) {
                 count = count + 1;
             }
         }
--- SQRService/src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ SQRService/src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,13 +2,15 @@ package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
 
 
     @ParameterizedTest
-    @CsvSource (value = {"`200-300`, 200, 300, 3 ", "`200-500`, 200, 500, 8"})
+    @CsvSource (value = {"`200-300`, 200, 300, 3 ",
+            "`200-500`, 200, 500, 8"})
     public void shouldSqrtCeil(String testName, int lowNum, int highNum, int expected) {
         SQRService service = new SQRService();
         int actual = service.sqrtCeil(lowNum, highNum);
